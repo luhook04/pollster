d7eea99ee9de97bd9ffa7ce6e5d844048c5a71c1
Luke Hook <lukehook@Lukes-MacBook-Pro.local>
2022-11-04 15:18:44 -0400

    Create the AuthContext that will allow me to login and logout from anywhere on the app
    Add the useEffect hook so that the user will be authenticated right away
    when logged in.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,36 @@
-import React from 'react';
+import React, { createContext, useEffect, useReducer } from 'react';
 import './App.css';
 import Header from './components/Header';
+import authReducer from './authReducer';
+
+const AuthContext = createContext();
 
 const App = () => {
+  interface InitStateValue {
+    isAuthenticated: boolean;
+    token: string;
+  }
+
+  const initialState: InitStateValue = {
+    isAuthenticated: false,
+    token: null,
+  };
+
+  const [state, dispatch] = useReducer(authReducer, initialState);
+
+  useEffect(() => {
+    const token: string = localStorage.getItem('token');
+
+    token && dispatch({ type: 'login', payload: { token: token } });
+  });
+
   return (
-    <div className="App">
-      <Header></Header>
-    </div>
+    <AuthContext.Provider value={{ state, dispatch }}>
+      <div className="App">
+        <Header></Header>
+      </div>
+    </AuthContext.Provider>
   );
 };
 
-export default App;
+export { App, AuthContext };
